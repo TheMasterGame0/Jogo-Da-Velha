cb68a2f0c61ee20103e248cf1eb18d6308a8dd99
TheMasterGame0 <TheMasterGame0Contact@gmail.com>
2022-09-07 17:55:06 -0300

    Update Jogo da Velha.R

--- Jogo da Velha.R
+++ Jogo da Velha.R
@@ -1,33 +1,32 @@
 # Feito por TheMasterGame0(GitHub)
-#
-ValoresTabuleiro = matrix(0,nrow=3,ncol=3)
-TabuleiroJogador = matrix("-",nrow=3,ncol=3)
+
+ValoresTabuleiro = matrix(0, nrow = 3, ncol = 3)
+TabuleiroJogador = matrix("-", nrow = 3, ncol = 3)
 
 # Determina as condições de vitória.
-Vitoria = function(ValoresTabuleiro){
-  casos = c(colSums(ValoresTabuleiro), rowSums(ValoresTabuleiro), sum(diag(ValoresTabuleiro)), sum(ValoresTabuleiro[1,3] + ValoresTabuleiro[2,2] + ValoresTabuleiro[3,1]))
-  if (any(casos == 3)){
+Vitoria = function(ValoresTabuleiro) {
+  casos = c(colSums(ValoresTabuleiro), rowSums(ValoresTabuleiro), sum(diag(ValoresTabuleiro)), sum(ValoresTabuleiro[1, 3] + ValoresTabuleiro[2, 2] + ValoresTabuleiro[3, 1]))
+  if (any(casos == 3)) {
     print("X ganhou!")
-    return (FALSE)
-  }else if (any(casos == -3)){
+    return(FALSE)
+  }else if (any(casos == -3)) {
     print("O ganhou!")
-    return (FALSE)
-  }else{
-    return (TRUE)
+    return(FALSE)
+  }else {
+    return(TRUE)
   }
 }
 
 #Determina se a jogada é válida.
-Jogada = function(ValoresTabuleiro, X, Y){
-  if (X>=1 & X<=3 & Y>= 1 & Y<=3){
-    if (ValoresTabuleiro[X, Y] == 0){
-      return (TRUE)
+Jogada = function(ValoresTabuleiro, X, Y) {
+  if (X >= 1 && X <= 3 && Y >=  1 && Y <= 3) {
+    if (ValoresTabuleiro[X, Y] == 0) {
+      return(TRUE)
+    }else {
+      return(FALSE)
     }
-    else{
-      return (FALSE)
-    }
-  }else{
-    return (FALSE)
+  }else {
+    return(FALSE)
   }
 }
 
@@ -35,30 +34,29 @@ Vez = 1   #Quem começa
 i = 0
 #Loop principal utilizado para o funcionamento do jogo.
 Ganhou = Vitoria(ValoresTabuleiro)
-while (i<10 & Ganhou){
+while (i < 10 && Ganhou) {
   print(TabuleiroJogador)
-  if (Vez == 1){
-    Letra = 'X'
+  if (Vez == 1) {
+    Letra = "X"
     cat("É a vez do X:\n")
   }else{
-    Letra = 'O'
-    cat("É a vez do O:\n") 
+    Letra = "O"
+    cat("É a vez do O:\n")
   }
   X = as.integer(readline(prompt = "Linha: "))
   Y = as.integer(readline(prompt = "Coluna: "))
-  if (Jogada(ValoresTabuleiro, X, Y)){
+  if (Jogada(ValoresTabuleiro, X, Y)) {
     ValoresTabuleiro[X, Y] = Vez
     TabuleiroJogador[X, Y] = Letra
-    Vez = Vez*-1
-    i = i+1
+    Vez = Vez * -1
+    i = i + 1
     Ganhou = Vitoria(ValoresTabuleiro)
-  }
-  else{
+  }else {
     print("Essa posição não é valida\n")
   }
 }
 
 # Retorna que o Jogo finalizou sem um vencedor (Velha)
-if (Ganhou){
+if (Ganhou) {
   print("O Jogo deu Velha!")
 }
